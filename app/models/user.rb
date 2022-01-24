3b9fde1ad14eb7dd9864b6ef638b51aa8a2a77fc
Titus <tkivite@gmail.com>
2022-01-24 17:58:31 +0300

    added model validations and updated field types

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,6 @@ class User < ApplicationRecord
   has_many :preferred_brands, through: :user_preferred_brands, source: :brand
   has_many :recommendations, through: :user_car_recomendations, source: :car
 
-  #scope :user_perfect_match, ->(car) { where(car.user_label(self) == "perfect_match") }
-  #scope :user_preferred_brand, ->(car) { joins(:car).where(user.preferred_brands.include?(car.brand) == true) }
-
   #validations
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-
-  def car_selection
-
-    # On top it should be cars with perfect_match label. This cars have match with user preferred car brands ( user.preferred_brands.include?(car.brand) == true) and with preferred price (user.preferred_price_range.include?(car.price) == true).
-    # Then we should have good_match offers. These offers have the only user preferred car brands (user.preferred_brands.include?(car.brand) == true).
-    # Then goes top 5 cars suggested by external recommendation service API (they can also be matched as perfect and good matches).
-    # Then goes all other cars sorted by price (ASC).
-  end
 end
